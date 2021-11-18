ac56fb1f603b130d0e8c3873eff2820e629ac651
Maxime Charpentier <pro.maxime.charpentier@gmail.com>
2021-11-18 20:51:57 +0100

    black reformat

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ for csv_file in os.listdir("./data_clean"):
 
 # Set app title
 
-st.title('Exploration de la stratégie')
+st.title("Exploration de la stratégie")
 
 # Utils functions
+
 
 def create_path(stops):
     stops = stops.split(" > ")
@@ -26,7 +27,7 @@ def create_path(stops):
         city = cities[cities["city"] == stop].iloc[0]
         coordinates.append([city["lng"], city["lat"]])
     if len(coordinates) == 1:
-        coordinates = 2*coordinates
+        coordinates = 2 * coordinates
     return shp.geometry.LineString(coordinates)
 
 
@@ -39,17 +40,21 @@ def contain(orders, words):
 
 
 def check_and_see(warehouse, date):
-    sub_orders = orders[(orders["delivered_date"]==date) & (orders["from_warehouse"]==warehouse)]
+    sub_orders = orders[
+        (orders["delivered_date"] == date) & (orders["from_warehouse"] == warehouse)
+    ]
     st.sidebar.write(f"**{len(sub_orders)} commandes** le jour {date[:10]}")
 
     sub_routes = routes[routes["orders"].apply(contain, words=sub_orders["order_id"])]
     st.sidebar.write(f"**{len(sub_routes)} routes** le jour {date[:10]}")
-    
-    sub_routes.loc[:,"path"] = sub_routes["stops"].apply(create_path)
+
+    sub_routes.loc[:, "path"] = sub_routes["stops"].apply(create_path)
 
     sub_routes.loc[:, "fill_rate"] = sub_routes.loc[:, "fill_volume"] / 81.25
 
-    st.sidebar.write(f"Taux de remplissage moyen sur tous les trajets : **{sub_routes.loc[:, 'fill_rate'].mean()*100:.0f}%**")
+    st.sidebar.write(
+        f"Taux de remplissage moyen sur tous les trajets : **{sub_routes.loc[:, 'fill_rate'].mean()*100:.0f}%**"
+    )
 
     for k in range(len(sub_routes)):
         st.subheader(f"Route {k + 1}")
@@ -58,24 +63,42 @@ def check_and_see(warehouse, date):
         st.write(f"Villes desservies : **{s_df.iloc[0].stops}**")
         fig, ax = plt.subplots(figsize=(15, 15))
 
-        sub_cities = pd.merge(sub_orders[["delivery_location", "order_total_volume"]].groupby("delivery_location").agg(sum).reset_index(),
-                            cities, left_on="delivery_location", right_on="city")
+        sub_cities = pd.merge(
+            sub_orders[["delivery_location", "order_total_volume"]]
+            .groupby("delivery_location")
+            .agg(sum)
+            .reset_index(),
+            cities,
+            left_on="delivery_location",
+            right_on="city",
+        )
 
-        gdf_cities = gpd.GeoDataFrame(sub_cities, geometry=gpd.points_from_xy(sub_cities.lng, sub_cities.lat), crs=4326)
-        gdf_cities.to_crs(epsg=3857).plot(ax=ax, column="order_total_volume", markersize=200)#, legend=True)
+        gdf_cities = gpd.GeoDataFrame(
+            sub_cities,
+            geometry=gpd.points_from_xy(sub_cities.lng, sub_cities.lat),
+            crs=4326,
+        )
+        gdf_cities.to_crs(epsg=3857).plot(
+            ax=ax, column="order_total_volume", markersize=200
+        )  # , legend=True)
 
         gdf = gpd.GeoDataFrame(s_df, geometry=s_df.path, crs=4326)
         gdf.to_crs(epsg=3857).plot(ax=ax, column="n_units", linewidth=3)
 
-        cx.add_basemap(ax, source=cx.providers.CartoDB.Voyager)#, zoom=7)
-        ax.axis('off')
+        cx.add_basemap(ax, source=cx.providers.CartoDB.Voyager)  # , zoom=7)
+        ax.axis("off")
         ax.set_title("Localisation des villes")
         st.pyplot(fig)
 
+
 # Defining variables
 
-warehouse = st.sidebar.selectbox("Choisir un entrepôt", options=warehouses.warehouse_city.tolist())
-date = st.sidebar.selectbox("Choisir une date de livraison", options=orders.delivered_date.unique().tolist())
+warehouse = st.sidebar.selectbox(
+    "Choisir un entrepôt", options=warehouses.warehouse_city.tolist()
+)
+date = st.sidebar.selectbox(
+    "Choisir une date de livraison", options=orders.delivered_date.unique().tolist()
+)
 
 # Running app
 
